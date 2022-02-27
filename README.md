061b5288dbfc2719ef44cdb8c76d67e79c8381d1
KELLEN ANKER <kellenanker7@gmail.com>
2022-02-26 19:06:22 -0500

    checkpoint

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ docker build -t 630010311280.dkr.ecr.us-east-1.amazonaws.com/url-shortener-ui:la
 
 docker run --rm -p 80:80 630010311280.dkr.ecr.us-east-1.amazonaws.com/url-shortener-ui:latest
 
-aws ecr get-login-password --region us-east-1
+aws ecr get-login --region us-east-1
 
 docker push 630010311280.dkr.ecr.us-east-1.amazonaws.com/url-shortener-ui:latest
 ```
--- src/UrlForm.js
+++ src/UrlForm.js
@@ -1,22 +1,41 @@
 import {useState} from 'react';
 
 const UrlForm = () => {
-    const [longUrl, setLongUrl] = useState('https://www.google.com/search?q=really+long+google+search');
-    const [shortUrlId, setShortUrlId] = useState('');
+    const [longUrl, setLongUrl] = useState('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    const [shortUrl, setShortUrl] = useState('');
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
+    const [inProgress, setInProgress] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetch(`https://short.kellenanker.com/shorten?longUrl=${longUrl}`)
-            .then(data => {
-                console.log(data.json());
-                setShortUrlId(data.id);
+        setSuccess(false);
+        setError(false);
+        setInProgress(true);
+
+        fetch('https://kell.link', {
+            method: 'POST',
+            body: JSON.stringify({longUrl})
+        })
+            .then(async response => {
+                const data = await response.json();
+                if (!response.ok) {
+                    const error = (data && data.message) || response.statusText;
+                    setError(true);
+                    return Promise.reject(error);
+                }
+
+                console.log(data);
+                setShortUrl(data.short_url)
                 setSuccess(true);
-            }).catch((error) => {
-                console.error('Error:', error);
-                setError(true)
-            });
+                setError(false);
+            })
+            .catch(error => {
+                setSuccess(false);
+                setError(true);
+                console.error('Error', error);
+            })
+            .finally(() => { setInProgress(false); });
     }
 
     const handleChange = (e) => setLongUrl(e.target.value);
@@ -24,15 +43,13 @@ const UrlForm = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleChange} value={longUrl} type="text" name="longUrl" id="longUrl" />
+                <input style={{ width: '80%' }} onChange={handleChange} value={longUrl} type="text" name="longUrl" id="longUrl" />
+                <br/>
                 <input type="submit" value="Shorten" />
             </form>
-            {success &&
-                <p>Your short url: <a href={`https://short.kellenanker.com/s/${shortUrlId}`}>{`https://short.kellenanker.com/s/${shortUrlId}`}</a></p>
-            }
-            {error &&
-                <p>There was an error processing your request.</p>
-            }
+            { inProgress && <p>Working...</p> }
+            { success && <p>Your short url: <a target="_blank" rel="noreferrer" href={shortUrl}>{shortUrl}</a></p> }
+            { error && <p>There was an error processing your request.</p> }
         </div>
     );
 }
